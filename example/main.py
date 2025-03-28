7be099cc80629d104cf9a55302ba470f4436962f
Ricardo Robles <ricardo.r.f@hotmail.com>
2025-03-28 14:47:23 +0100

    relocate files

--- example/main.py
+++ example/main.py
@@ -1,3 +1,8 @@
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 from bunnyhopapi.server import Server
 from bunnyhopapi import logger
 from bunnyhopapi.models import PathParam
