af2534ae2994221528c1721f6a06982a3424c50a
Ricardo Robles <ricardo.r.f@hotmail.com>
2026-04-15 08:52:36 +0200

    bump version to 2.0.1 in pyproject.toml, setup.py, and uv.lock

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="bunnyhopapi",
-    version="2.0.0",
+    version="2.0.1",
     description="A simple, easy and fast http server for python",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
